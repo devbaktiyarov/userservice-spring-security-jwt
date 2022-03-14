93b419421d1ff2f61f00fec82107473355031c22
devbaktiyarov <devbaktiyarov@gmail.com>
2022-03-14 21:22:09 +0600

    Init commit

---
+++ src/main/java/com/arsenbaktiyarov/microservices/userservicespringsecurityjwt/UserserviceSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.arsenbaktiyarov.microservices.userservicespringsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserserviceSpringSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserserviceSpringSecurityJwtApplication.class, args);
+    }
+
+}
