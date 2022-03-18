3085d5031b4a1915cb90dd1f89f574f167fc5f1f
devbaktiyarov <devbaktiyarov@gmail.com>
2022-03-18 16:36:38 +0600

    Create refresh endpoint

--- src/main/java/com/arsenbaktiyarov/microservices/userservicespringsecurityjwt/config/SecurityConfig.java
+++ src/main/java/com/arsenbaktiyarov/microservices/userservicespringsecurityjwt/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.arsenbaktiyarov.microservices.userservicespringsecurityjwt.config;
 
 import com.arsenbaktiyarov.microservices.userservicespringsecurityjwt.filter.CustomAuthenticationFilter;
+import com.arsenbaktiyarov.microservices.userservicespringsecurityjwt.filter.CustomAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +23,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private final UserDetailsService userDetailsService;
 	private final BCryptPasswordEncoder passwordEncoder;
-
 
 	public SecurityConfig(@Qualifier("userServiceImpl") UserDetailsService userDetailsService
 			, BCryptPasswordEncoder passwordEncoder) {
@@ -36,10 +38,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		CustomAuthenticationFilter filter = new CustomAuthenticationFilter(authenticationManagerBean());
+		filter.setFilterProcessesUrl("/api/login");
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
-		http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+		http.authorizeRequests().antMatchers("/api/login/**", "/api/v1/users/token/refresh").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/users/**").hasAuthority("ROLE_USER");
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/users/**").hasAuthority("ROLE_ADMIN");
+		http.authorizeRequests().anyRequest().authenticated();
+		http.addFilter(filter);
+		http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	@Bean
--- src/main/java/com/arsenbaktiyarov/microservices/userservicespringsecurityjwt/contoller/UserController.java
+++ src/main/java/com/arsenbaktiyarov/microservices/userservicespringsecurityjwt/contoller/UserController.java
@@ -3,13 +3,33 @@ package com.arsenbaktiyarov.microservices.userservicespringsecurityjwt.contoller
 import com.arsenbaktiyarov.microservices.userservicespringsecurityjwt.domain.Role;
 import com.arsenbaktiyarov.microservices.userservicespringsecurityjwt.domain.User;
 import com.arsenbaktiyarov.microservices.userservicespringsecurityjwt.service.UserService;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
@@ -38,6 +58,39 @@ public class UserController {
     public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
         userService.addRoleToUser(form.getUsername(), form.getRoleName());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("JWT ")) {
+            try {
+                String refreshToken = authorizationHeader.substring("JWT ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refreshToken);
+                String username = decodedJWT.getSubject();
+                User user = userService.getUser(username);
+                String accessToken = JWT.create().withSubject(user.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 60000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles", user.getRoles().stream().map(Role::getName)
+                                .collect(Collectors.toList()))
+                        .sign(algorithm);
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", accessToken);
+                tokens.put("refresh_token", refreshToken);
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+            } catch (Exception e) {
+                response.setHeader("error", e.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                Map<String, String> errors = new HashMap<>();
+                errors.put("error_message", e.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), errors);
+            }
+        }
     }
 }
 
